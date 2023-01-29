9781c47fd92346487a1043b123e1a4a198be075c
Nick <ncerda9586@gmail.com>
2023-01-28 21:33:06 -0600

    Reconfiged file for new plugin test.

--- src/main/java/com/helmetcheck/HelmetCheckPlugin.java
+++ src/main/java/com/helmetcheck/HelmetCheckPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.helmetcheck;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Plugin that checks if you're wearing your helmet."
 )
-public class ExamplePlugin extends Plugin
+public class HelmetCheckPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private HelmetCheckConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	HelmetCheckConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(HelmetCheckConfig.class);
 	}
 }
